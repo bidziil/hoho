c88a2569517194bd7270e51fd949a0ed6ce3afe6
MokusKocka <MokusKocka@DESKTOP-5VQP1F2>
2019-06-25 21:37:35 +0200

    initial html structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { IntroductionComponent } from './view/introduction.component';
+import { ReferenceComponent } from './view/reference.component';
+
+const routes: Routes = [
+  // {
+  //   path: '**',
+  //   component: IntroductionComponent,
+  //   data: { title: 'Introduction' }
+  // },
+  {
+    path: 'introduction',
+    component: IntroductionComponent,
+    data: { title: 'Introduction' }
+  },
+  {
+    path: 'reference',
+    component: ReferenceComponent,
+    data: { title: 'Reference' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {
+  ConfirmationService, MessageService,
+  AutoCompleteModule, RadioButtonModule, AccordionModule, InputTextModule,
+  DataGridModule, DataScrollerModule, DialogModule, SharedModule,
+  SliderModule, ChartModule, SelectButtonModule, MessageModule,
+  FieldsetModule, TreeModule, PaginatorModule, CardModule, MessagesModule,
+  InplaceModule, SplitButtonModule, ToggleButtonModule, ListboxModule,
+  DataListModule, FileUploadModule, PickListModule, ProgressBarModule,
+  DeferModule, CheckboxModule, TriStateCheckboxModule, DropdownModule,
+  MenubarModule, OverlayPanelModule, PanelModule, PanelMenuModule, SpinnerModule,
+  TabViewModule, ToolbarModule, MultiSelectModule, ButtonModule, CalendarModule,
+  ConfirmDialogModule, InputTextareaModule, GalleriaModule
+}                                                               from 'primeng/primeng';
+import { TableModule }                                          from 'primeng/table';
+import { TreeTableModule }                                      from 'primeng/treetable';
+import { ToastModule }                                          from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IntroductionComponent } from './view/introduction.component';
+import { ReferenceComponent } from './view/reference.component';
+import { MenuComponent } from './menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    ReferenceComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    // primeng modules:
+    AutoCompleteModule, RadioButtonModule, AccordionModule, InputTextModule,
+    DataGridModule, DataScrollerModule, DialogModule, SharedModule,
+    SliderModule, ChartModule, SelectButtonModule, MessageModule,
+    FieldsetModule, TreeModule, PaginatorModule, CardModule, MessagesModule,
+    InplaceModule, SplitButtonModule, ToggleButtonModule, ListboxModule,
+    DataListModule, FileUploadModule, PickListModule, ProgressBarModule,
+    DeferModule, CheckboxModule, TriStateCheckboxModule, DropdownModule,
+    MenubarModule, OverlayPanelModule, PanelModule, PanelMenuModule, SpinnerModule,
+    TabViewModule, ToolbarModule, MultiSelectModule, ButtonModule, CalendarModule,
+    ConfirmDialogModule, TableModule, TreeTableModule, ToastModule,
+    InputTextareaModule, GalleriaModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ConfirmationService
+    , MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
