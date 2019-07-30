968927a5d259c99a0ef7933954f6c7bbc081a9ee
Zoltan Szabo <ZSzabo12@hu.ibm.com>
2019-07-30 16:12:00 +0200

    config RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,9 @@ import { PicturesComponent } from './view/pictures.component';
 import { ContactComponent } from './view/contact.component';
 import { ProfilesComponent } from './view/profiles.component';
 import { DownloadsComponent } from './view/downloads.component';
+import { environment } from 'src/environments/environment';
 
 const routes: Routes = [
-  {
-    path: '**',
-    component: IntroductionComponent,
-    data: { title: 'Introduction' }
-  },
   {
     path: 'introduction',
     component: IntroductionComponent,
@@ -43,11 +39,17 @@ const routes: Routes = [
     path: 'downloads',
     component: DownloadsComponent,
     data: { title: 'downloads' }
+  },
+  // otherwise redirect to home
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false, enableTracing: !environment.production})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
